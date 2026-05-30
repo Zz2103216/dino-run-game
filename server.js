f28833a89bc0a3f5ff756d1368f0b7a2be54ac6a
zhangjinglei <zhangjinglei@EricdeMacBook-Air.local>
2026-05-30 13:24:07 +0800

    联机大厅重做：颜色选择器+玩家名称+房间管理+单人双人图标入口

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,13 +9,21 @@ const io = new Server(server);
 app.use(express.static(__dirname));
 
 // 房间管理
-const rooms = {}; // { roomCode: { players: [{id, name}], state: 'waiting'|'playing' } }
+const rooms = {}; // { code: { players: [{id, name, colorIdx, score, kills, alive, isHost}], state } }
 
 function generateRoomCode() {
   let code;
   do { code = String(Math.floor(1000 + Math.random() * 9000)); }
   while (rooms[code]);
   return code;
+}
+
+function broadcastRoom(roomCode) {
+  const room = rooms[roomCode];
+  if (!room) return;
+  io.to(roomCode).emit('room-updated', {
+    players: room.players.map(p => ({ id: p.id, name: p.name, colorIdx: p.colorIdx, isHost: p.isHost }))
+  });
 }
 
 io.on('connection', (socket) => {
@@ -26,34 +33,64 @@ io.on('connection', (socket) => {
   socket.on('create-room', () => {
     const code = generateRoomCode();
     rooms[code] = {
-      players: [{ id: socket.id, score: 0, kills: 0, alive: true }],
+      players: [{ id: socket.id, name: '', colorIdx: 0, score: 0, kills: 0, alive: true, isHost: true }],
       state: 'waiting'
     };
     socket.join(code);
     socket.roomCode = code;
-    socket.emit('room-created', { code });
-    console.log(`[房间] ${code} 已创建`);
+    socket.emit('room-created', {
+      code,
+      players: rooms[code].players.map(p => ({ id: p.id, name: p.name, colorIdx: p.colorIdx, isHost: p.isHost }))
+    });
+    console.log(`[房间] ${code} 已创建 (房主: ${socket.id})`);
   });
 
   // 加入房间
-  socket.on('join-room', (code) => {
+  socket.on('join-room', (data) => {
+    const { code, name, colorIdx } = data;
     const room = rooms[code];
-    if (!room) {
-      socket.emit('join-error', '房间不存在');
-      return;
-    }
-    if (room.players.length >= 2) {
-      socket.emit('join-error', '房间已满');
-      return;
-    }
-    room.players.push({ id: socket.id, score: 0, kills: 0, alive: true });
-    room.state = 'playing';
+    if (!room) { socket.emit('join-error', '房间不存在'); return; }
+    if (room.players.length >= 2) { socket.emit('join-error', '房间已满'); return; }
+    if (room.state === 'playing') { socket.emit('join-error', '游戏已开始'); return; }
+
+    room.players.push({
+      id: socket.id, name: name || '玩家2', colorIdx: colorIdx || 0,
+      score: 0, kills: 0, alive: true, isHost: false
+    });
     socket.join(code);
     socket.roomCode = code;
     socket.emit('room-joined', { code });
-    // 通知双方开始
-    io.to(code).emit('game-start', { playerCount: room.players.length });
-    console.log(`[房间] ${code} 玩家加入，开始对战`);
+    broadcastRoom(code);
+    console.log(`[房间] ${code} ${name} 加入`);
+  });
+
+  // 更新玩家信息
+  socket.on('player-info', (data) => {
+    const code = socket.roomCode;
+    if (!code || !rooms[code]) return;
+    const player = rooms[code].players.find(p => p.id === socket.id);
+    if (player) {
+      if (data.name) player.name = data.name;
+      if (data.colorIdx !== undefined) player.colorIdx = data.colorIdx;
+    }
+    broadcastRoom(code);
+  });
+
+  // 房主请求开始游戏
+  socket.on('request-start', () => {
+    const code = socket.roomCode;
+    if (!code || !rooms[code]) return;
+    const player = rooms[code].players.find(p => p.id === socket.id);
+    if (!player || !player.isHost) return;
+    if (rooms[code].players.length < 2) return;
+
+    rooms[code].state = 'playing';
+    io.to(code).emit('game-start', {
+      players: rooms[code].players.map(p => ({
+        id: p.id, name: p.name, colorIdx: p.colorIdx
+      }))
+    });
+    console.log(`[房间] ${code} 游戏开始`);
   });
 
   // 玩家状态同步
@@ -73,10 +110,10 @@ io.on('connection', (socket) => {
   });
 
   // 射击
-  socket.on('player-shoot', (data) => {
+  socket.on('player-shoot', () => {
     const code = socket.roomCode;
     if (!code || !rooms[code]) return;
-    socket.to(code).emit('remote-shoot', data);
+    socket.to(code).emit('remote-shoot', {});
   });
 
   // 玩家死亡
@@ -86,30 +123,31 @@ io.on('connection', (socket) => {
     const player = rooms[code].players.find(p => p.id === socket.id);
     if (player) player.alive = false;
     socket.to(code).emit('remote-died', data);
-    // 检查对方是否也死了
-    const allDead = rooms[code].players.every(p => !p.alive);
-    if (allDead) {
-      io.to(code).emit('game-draw', { players: rooms[code].players.map(p => ({ id: p.id, score: p.score, kills: p.kills })) });
-    }
   });
 
   // Boss 击杀
-  socket.on('player-boss-kill', (data) => {
+  socket.on('player-boss-kill', () => {
     const code = socket.roomCode;
     if (!code || !rooms[code]) return;
-    socket.to(code).emit('remote-boss-kill', data);
+    socket.to(code).emit('remote-boss-kill', {});
   });
 
-  // 断开连接
+  // 断开
   socket.on('disconnect', () => {
     console.log(`[断开] ${socket.id}`);
     const code = socket.roomCode;
     if (code && rooms[code]) {
       rooms[code].players = rooms[code].players.filter(p => p.id !== socket.id);
-      socket.to(code).emit('opponent-left');
       if (rooms[code].players.length === 0) {
         delete rooms[code];
         console.log(`[房间] ${code} 已删除`);
+      } else {
+        // 转移房主
+        if (!rooms[code].players.find(p => p.isHost)) {
+          rooms[code].players[0].isHost = true;
+        }
+        socket.to(code).emit('opponent-left');
+        broadcastRoom(code);
       }
     }
   });
